ed3e4d71d8e9f67aa3e46acc5a6e8c4a1422c653
Konstantin0305 <kgrishin@yahoo.com>
2022-10-18 17:27:15 +0400

    base and main page added, test_main_page modified

--- test_main_page.py
+++ test_main_page.py
@@ -1,3 +1,4 @@
+from .pages.main_page import MainPage
 from selenium.webdriver.common.by import By
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
@@ -7,8 +8,13 @@ def test_button_add_to_basket(browser):
     button = browser.find_elements(By.CSS_SELECTOR, ".btn.btn-lg.btn-primary")
     assert button, "Button is not found"
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
+
+def go_to_login_page(browser):
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
     login_link.click()
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
